ae2cd5aad229a4d56155f57347a4bcc27b4f742b
nipun221 <2oics072@gbu.ac.in>
2023-09-01 23:16:42 +0530

    first commit

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,7 @@
 import Searchbar from "./Searchbar"
+import Cart from "../assets/icons8-cart-30.png"
+import User from "../assets/icons8-male-user-50.png"
+import Github from "../assets/icons8-github-64.png"
 
 const Nav = () => {
   return (
@@ -8,11 +11,38 @@ const Nav = () => {
             <Searchbar/>
             <div className='flex justify-between items-center p-4'>
                 <a href="/">
-
-                    <h2 className='text-[15px] font-mono text-black font-[600] p-2'>Cart</h2>
+                    <p className='text-[15px] font-mono text-black font-[600] p-2'>
+                        <img 
+                            src={Cart}
+                            alt="Cart"
+                            width={28}
+                            height={28}
+                        />
+                        Cart 
+                    </p>
                 </a>
-                <a href="/"><h2 className='text-[15px] font-mono text-black font-[600] p-4'>Profile</h2></a>
-                <a href="/"><h2 className='text-[15px] font-mono text-black font-[600] p-4'>GitHub</h2></a>
+                <a href="/">
+                    <p className='text-[15px] font-mono text-black font-[600] p-2'>
+                        <img 
+                            src={User}
+                            alt="User"
+                            width={28}
+                            height={28}
+                        />
+                        User
+                    </p>
+                </a>
+                <a href="https://github.com/nipun221/e-commerce">
+                    <h2 className='text-[15px] font-mono text-black font-[600] p-2'>
+                        <img 
+                            src={Github}
+                            alt="Github"
+                            width={28}
+                            height={28}
+                        />
+                        GitHub
+                    </h2>
+                </a>
             </div>
         </nav>
     </header>
