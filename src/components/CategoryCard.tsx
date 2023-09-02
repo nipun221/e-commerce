7aa8c94620ac8035ef08579389be5be2d9eb3b54
nipun221 <2oics072@gbu.ac.in>
2023-09-02 23:26:38 +0530

    first commit

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -6,13 +6,13 @@ interface Props {
 
 const CategoryCard: React.FC<Props> = ({ image, name, count }) => {
   return (
-    <div className="border border-gray-200 hover:border-gray-400 hover:scale-105 transition-transform rounded-lg">
+    <div className="w-[350px] h-[100px] border border-gray-200 hover:border-gray-400 hover:scale-105 transition-transform rounded-lg">
         <div className="flex justify-between items-center">
             <div className="space-y-4">
-                <h3 className="font-medium text-xl">{name}</h3>
-                <p className="text-gray-500">{count}</p>
+                <h3 className="font-medium text-xl text-center mt-2 px-4">{name}</h3>
+                <p className="text-gray-500 text-center">{count}</p>
             </div>
-            <img src={image} alt={name} className="w-20 h-20 object-cover rounded-full" />
+            <img src={image} alt={name} className="w-[100px]" />
         </div>
     </div>
   )
