735ebaefbb21c60df818d40ca0a347082a9a815f
nipun221 <2oics072@gbu.ac.in>
2023-09-02 23:31:31 +0530

    first commit

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
   return (
     <header className='sticky top-0 padding-x absolute z-10 w-full bg-white'>
         <nav className="flex justify-between items-center h-[60px] max-container">
-            <a href="/"><span className='text-2xl font-sans font-[650] p-6 bg-gradient-to-r text-transparent bg-clip-text from-purple-700 to-red-500'>LaptopHub</span></a>
+            <a href="/"><span className='text-2xl font-mono font-[650] p-6 bg-gradient-to-r text-transparent bg-clip-text from-blue-700 to-purple-500'>GamingHub.com</span></a>
             <Searchbar/>
             <div className="flex gap-4 p-6">
                 <div className="icon__wrapper">
